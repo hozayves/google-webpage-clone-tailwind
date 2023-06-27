182d94f20a84b232e1d365a2dc6ca3be238700e1
hozayves <muhozayves2014@gmail.com>
2023-06-27 22:27:19 +0200

    Make a changes

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './style.css'
+const appBtn = document.querySelector('#googleApp');
+const appModal = document.querySelector("[role='google-app']");
+
+const googleAccount = document.querySelector('#gAccount');
+const googleModal = document.querySelector("[role='google-account']")
+
+appBtn.addEventListener('click', (e) => {
+    let isExpanded = JSON.parse(appBtn.getAttribute('aria-expanded'));
+    appBtn.setAttribute('aria-expanded', !isExpanded);
+    appModal.classList.toggle('hidden');
+    appModal.classList.toggle('flex')
+});
+googleAccount.addEventListener('click', (e) => {
+    let isExpanded = JSON.parse(googleAccount.getAttribute('aria-expanded'));
+    googleAccount.setAttribute('aria-expanded', !isExpanded);
+    googleModal.classList.toggle('hidden');
+    googleModal.classList.toggle('flex')
+})
